98881149d1abc6eb9bfa09d14d94ab2fa80f2989
Jairo <jairo.adame@gmail.com>
2021-06-04 11:49:24 -0500

    Homework done!

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -99,9 +99,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if (numero === Math.floor(numero)) {
-    return true;
-  } else return false;
+  // if (numero === Math.floor(numero)) {
+  //   return true;
+  // } else return false;
+  return Number.isInteger(numero);
+  // return Math.floor(numero) === numero;
+  // return numero % 1 === 0;
 }
 
 function fizzBuzz(numero) {
@@ -143,21 +146,23 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero === 0 || numero === 1) {
+  // if (numero === 0 || numero === 1) {
+  if (numero < 2) {
     return false;
   }
   for (let i = 2; i < numero; i++) {
     if (numero % i === 0) {
       return false;
-    } else return true;
-  } 
+    } 
+  } return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor === true) {
+  // if (valor === true) {
+  if (valor) {
     return 'Soy verdadero';
   } else return 'Soy falso';
 
@@ -177,15 +182,26 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.length = 3) {
-    return true;
-  } else return false;
+  var cantidadDeDigitos = 0;
+  while(Math.floor(numero)!=0){
+    numero = numero / 10;
+    cantidadDeDigitos ++;
+  }
+  if(cantidadDeDigitos === 3) return true;
+  else return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var vecesIterando = 0;
+  do {
+    // numero = numero + 5;
+    numero += 5;
+    vecesIterando++;
+  } while (vecesIterando < 8);
+  return numero;
 }
 
 
